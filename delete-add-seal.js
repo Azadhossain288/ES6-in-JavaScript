d08573be2cf8130d22be1feb21fd12d306b3f5c6
Azadhossain288 <azadhossain016288@gmail.com>
2026-03-01 20:23:03 +0600

    Added delete,freez,keys,value

---
+++ delete-add-seal.js
@@ -0,0 +1,11 @@
+const king={name:'Mufasa',age:55,kingdom:'Pride Lands'};
+delete king.age;
+delete king.name;
+
+// add 
+
+king.queen='Sarabi';
+king.name='King Simba';
+
+console.log(king);
+
---
+++ keys-values.js
@@ -0,0 +1,9 @@
+const king={name:'Mufasa',age:54,kingdom:'Pride Lands'};
+const keys=Object.keys(king);
+console.log(keys);
+
+const values=Object.values(king);
+console.log(values);
+
+const entries=Object.entries(king);// give even 
+console.log(entries);
